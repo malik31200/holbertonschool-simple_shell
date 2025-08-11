7a1767955ef3f71e6fb660c238691570f7014b90
AdelMej <adelmejrissi@gmail.com>
2025-08-11 19:11:58 +0200

    fix: removed a semi colon

--- string.c
+++ string.c
@@ -12,12 +12,13 @@
 int _strcmp(char *str1, char *str2)
 {
 	size_t i = 0;
+
 	for (i = 0; str1[i] != '\0' && str2[i] != '\0'; i++)
 	{
 		if (str1[i] != str2[i])
 			return (str1[i] - str2[i]);
 	}
-	return (str1[i] - str2[i]);;
+	return (str1[i] - str2[i]);
 }
 
 /**
