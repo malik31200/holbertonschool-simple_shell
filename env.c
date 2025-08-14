1a81346869ba3f9ae3c375e17b374c134a81eb66
AdelMej <adelmejrissi@gmail.com>
2025-08-14 17:53:00 +0200

    feat: made environ utilities

---
+++ env.c
@@ -0,0 +1,38 @@
+#include "env.h"
+#include <string.h>
+
+/**
+* get_env - returns the environement system variable
+*
+* Return: the environement system variable if it succeed
+* NULL if it fails
+*/
+char **get_env(void)
+{
+	return (environ);
+}
+
+/**
+* get_env_var - searches for a vairable in environ
+* @var: the variable to search
+*
+* Return: the string containing the variable
+* NULL if it fails
+*/
+char *get_env_var(const char *var)
+{
+	char **environ = get_env();
+	size_t i, len;
+
+	if (var == NULL || environ == NULL)
+		return (NULL);
+
+	len = strlen(var);
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		if(strncmp(var, environ[i], len) == 0 && environ[i][len] == '=')
+			return (environ[i]);
+	}
+
+	return (NULL);
+}
---
+++ env.h
@@ -0,0 +1,12 @@
+#ifndef MY_ENV_H
+#define MY_ENV_H
+
+/* system variable environ */
+extern char **environ;
+
+/* --- environ utilities --- */
+char **get_env(void);
+char *get_env_var(const char *var);
+
+
+#endif /* MY_ENV_H */
